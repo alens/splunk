fe8f68f686b3906554c5b41b5c8d3f41f0365eb0
AlenS <alens@users.noreply.github.com>
2023-11-24 19:43:08 +0200

    Update Splunk_UniversalForwarder_Setup.sh
    added a pause and systemd

--- Splunk_UniversalForwarder_Setup.sh
+++ Splunk_UniversalForwarder_Setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ### Variables section
-SPLUNK_UF_VERSION_RPM="splunkforwarder-9.1.1-64e843ea36b1.x86_64.rpm"
-SPLUNK_UF_VERSION_DEB="splunkforwarder-9.1.1-64e843ea36b1-linux-2.6-amd64.deb"
-DEPLOYMENT_SERVER=10.1.1.103 #Replace with the name/ip of your deployment server
+SPLUNK_UF_VERSION_RPM="splunkforwarder-9.1.2.x86_64.rpm"
+SPLUNK_UF_VERSION_DEB="splunkforwarder-9.1.2-linux-2.6-amd64.deb"
+DEPLOYMENT_SERVER=10.10.10.10 #Replace with the name/ip of your deployment server
 DEPLOYMENT_PORT=8089 
 INSTALL_DIR='/opt' 
-SPLUNK_TEMPADMINPASS=changeme 
+SPLUNK_TEMPADMINPASS=StrongPassword123 #change to something  
 
 ### End variables
 
@@ -72,8 +72,11 @@ $INSTALL_DIR/splunkforwarder/bin/splunk start --no-prompt --accept-license --ans
 
 # Enable boot-start
 echo "Enabling boot-start"
-$INSTALL_DIR/splunkforwarder/bin/splunk enable boot-start -systemd-managed 0
+$INSTALL_DIR/splunkforwarder/bin/splunk enable boot-start -systemd-managed 1   #0 for init.d 1 for systemd
 
+# Pause for 5 sec
+echo "Pausing for 5 seconds..."
+sleep 5
 
 # Check if the Splunk UF service is running properly
 echo "Validating Splunk UF installation..."
